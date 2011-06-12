b6ee26f8f7a090882c6e6962436475d5347e3877
Paul Annesley <paul@annesley.cc>
2011-06-12 19:35:31 +1000

    Example routes.rb (example_routes.rb), routes.rb is in .gitignore list.

---
+++ example_routes.rb
@@ -0,0 +1,12 @@
+Router.instance.reset.tap do |r|
+
+  r.backends = {
+    rails:  "127.0.0.1:3000",
+    static: "127.0.0.1:8080",
+  }
+
+  r.connect :static,
+    %r{^/robots.txt},
+    %r{^/(?:images|stylesheets|javascripts)/}
+
+end
